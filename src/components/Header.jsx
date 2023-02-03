34ab11e6d998e33bb1d9af2341b969db3ab1054d
Taryn King <marshaeking@gmail.com>
2023-02-03 09:15:57 -0600

    style: add header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 const Header = () => {
     return ( 
         <div className={styles.header}>
-            <h2>👑 King's Movie App</h2>
+            <h2>Movie Watch List</h2>
         </div>
      );
 }
